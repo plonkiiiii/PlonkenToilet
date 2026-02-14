680b8be55f31a48435cc0a0f5b9408beab3fcbdc
plonk <virosav@gmail.com>
2026-02-14 12:31:14 +0100

    Create Plonk Shit

---
+++ FrankenToilet/Plonk/MainCode.cs
@@ -0,0 +1,32 @@
+﻿using FrankenToilet.Core;
+using HarmonyLib;
+using UnityEngine;
+
+namespace FrankenToilet.Plonk;
+
+[EntryPoint]
+public static class MainCode
+{
+    [EntryPoint]
+    public static void Start()
+    {
+      GameObject obj = new GameObject("abc");
+      obj.AddComponent<Penis>();
+    }
+    public static float gravitySwap = 0f;
+    [PatchOnEntry]
+    [HarmonyPrefix, HarmonyPatch(typeof(NewMovement), nameof(NewMovement.Jump))]
+    public static void FuckingFuckGravity(NewMovement __instance)
+    {
+        Vector3 gravDir = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+        Physics.gravity = gravDir * 40f;
+        gravitySwap = Random.Range(1f, 10f);
+    }
+} 
+
+public class Penis : MonoBehaviour
+{  
+    public void Start() { gameObject.hideFlags = HideFlags.HideAndDontSave; DontDestroyOnLoad(this.gameObject); }
+
+    public void Update() => MainThingy.gravitySwap -= Time.deltaTime;
+}
